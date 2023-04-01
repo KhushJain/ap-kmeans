e5f143211cb6e3b0fecc4c70d3ba23de53f5d8df
maximuusS <ksaurav0211@gmail.com>
2023-04-01 19:40:55 -0400

    Fixed the code and created a X variable with all the coordinates

--- src/AppSTD.js
+++ src/AppSTD.js
@@ -1,15 +1,25 @@
 import React, { useEffect, useState, useMemo } from 'react';
 import axios from 'axios';
-import RawPositioning from './RawPositioning.js'
+//import RawPositioning from './RawPositioning.js'
 // import Radviz from './RadvizSTD.js'
-import 'rc-slider/assets/index.css';
+//import 'rc-slider/assets/index.css';
 
-export default function App() {
+export default function AppSTD() {
 
 	const [rawData, setRawData] = useState([])
 	const [data, setData] = useState({});
 	const [hoverKey, setHoverKey] = useState(-1)
+	const [X, setX] = useState([])
+	const loadData = async () => {
+			let res = await axios('./Data/postmapping.json')
+			let data = res.data.map((d, i) => ({key: i, ...d}))
 
+			//getting all the values of x and y coordinates
+			const temp = data.map((d, i) => ({key: i, x: d.coordinates.x, y: d.coordinates.y}))
+			
+			setRawData(data)
+			setX(temp)
+		}
 	let labelMapping = {
 		'Democratic Seat Share': 'dem seat',
 		'Efficiency Gap': 'eff gap',
@@ -26,34 +36,39 @@ export default function App() {
 		'Population Equality': 350
 	})
 
-	useEffect(async () => {
-		let res = await axios('./Data/premapping.json')
-		let data = res.data.map((d, i) => ({key: i, ...d}))
-		setRawData(data)
+	useEffect(() => {
+		
+		loadData()
+		
+		//creating variable x with all x and y coordinates
+		//const temp1 = rawData.map((d, i) => [d.x, d.y])
+	
+		//setX(temp)
 	}, [])
 
-	useEffect(() => {
 
-		// Statistical and Regualr require different label Mappings.
-		let { points, labels, std } = RawPositioning(rawData, labelMapping, labelAngles, 'key')
-		// downloadObjectAsJson(points, 'text')
-		setData({ points, labels, std })
-	}, [labelAngles, rawData])
+	// useEffect(() => {
+
+	// 	// Statistical and Regualr require different label Mappings.
+	// 	//let { points, labels, std } = RawPositioning(rawData, labelMapping, labelAngles, 'key')
+	// 	// downloadObjectAsJson(points, 'text')
+	// 	//setData({ points, labels, std })
+	// }, [labelAngles, rawData])
 
 
-	function downloadObjectAsJson(list, exportName){
-		var json = JSON.stringify(list);
-		var blob = new Blob([json], {type: 'application/json'});
-		var link = document.createElement('a');
-		link.href = window.URL.createObjectURL(blob);
-		link.download = exportName;
-		link.click();
-	  }
+	// function downloadObjectAsJson(list, exportName){
+	// 	var json = JSON.stringify(list);
+	// 	var blob = new Blob([json], {type: 'application/json'});
+	// 	var link = document.createElement('a');
+	// 	link.href = window.URL.createObjectURL(blob);
+	// 	link.download = exportName;
+	// 	link.click();
+	//   }
 
 
 	return (
 		<div>
-			<div style={{ width: '30%', height: '100%', position: 'fixed', padding: '5px' }}>
+			{/* <div style={{ width: '30%', height: '100%', position: 'fixed', padding: '5px' }}>
 				<div id='sidebar'>
 					{useMemo(() => <Radviz
 						points={data.points}
@@ -62,7 +77,8 @@ export default function App() {
 						hoverOver={setHoverKey}
 						showHSV={true} />, [data, hoverKey])}
 				</div>
-			</div>
+			</div> */}
+			Hello
 		</div >
 	);
 }
